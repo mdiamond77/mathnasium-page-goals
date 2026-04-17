23acac9f8d255716b5d965e6e569b98df0f727f0
mdmathnasiums <mdmathnasiums@gmail.com>
2026-04-16 21:00:38 -0400

    feat: initial scaffold

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+RADIUS_LOGIN_URL = "https://radius.mathnasium.com"
+# RADIUS_DWP_URL filled in during Task 3 (Radius investigation)
+RADIUS_DWP_URL = ""
+
+DRIVE_FOLDER_ID = "1NcVaeoFtyJkJfy6-GtLrlMxoN5cT_gyY"
+
+SUCCESS_RECIPIENTS = [
+    "matt.diamond@mathnasium.com",
+    "teaneck@mathnasium.com",
+    "englewood@mathnasium.com",
+]
+ERROR_RECIPIENTS = ["matt.diamond@mathnasium.com"]
+
+INPUT_DIR = "input"
+OUTPUT_DIR = "output"
+RUN_LOG_PATH = "run_log.json"
